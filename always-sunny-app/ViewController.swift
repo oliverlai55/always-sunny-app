218d3d74860222f4ebd4c8cad34d218087723a9e
oliverlai55 <oliverlai55@gmail.com>
2015-11-30 21:19:04 -0500

    always-sunny-app

---
+++ always-sunny-app/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  always-sunny-app
+//
+//  Created by Wei lun Lai on 11/30/15.
+//  Copyright © 2015 DigitalCrafts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBOutlet weak var tableView: UITableView!
+}
+
